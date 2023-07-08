4d3c38f28b6bfa06e080ddef908a88497c329744
Moritz Mantel <admin@moritz-mantel.de>
2023-07-08 14:57:05 +0200

    Changed Name

--- js/designesia.js
+++ js/designesia.js
@@ -1,5 +1,5 @@
  /* --------------------------------------------------
-  * © Copyright 2021 - Hostco by Designesia
+  * © Copyright 2021 - Servermanagementpanel by NerdsCave
   * --------------------------------------------------*/
 (function($) {
 	'use strict';
